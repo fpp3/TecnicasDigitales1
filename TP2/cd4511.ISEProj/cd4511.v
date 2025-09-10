d39e4a64bf8c47ab3fe143e02e5acf701b2bacb8
Franco Palombo <ffpp.2003@hotmail.com>
2025-09-10 19:21:46 -0300

    tp2: termina el informe

--- TP2/cd4511.ISEProj/cd4511.v
+++ TP2/cd4511.ISEProj/cd4511.v
@@ -1,52 +1,24 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:23:49 08/28/2025 
-// Design Name: 
-// Module Name:    cd4511 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module cd4511(							// desc				loglev	precedence
-  input  		[3:0] BCD,			// data				-			3
-  input			LightTest,			// light test 		(inv)		2
-  input 	 		BLanking,			// blanking 		(inv)		1
-  input 			LatchEnable,	// latch enable	-			0
-  output			COMC,
-  output reg 	[6:0] SEG			// 7-seg out		-			-
+
+module cd4511(
+  input       [3:0] BCD,
+  output      COMC,
+  output reg  [6:0] SEG 
 );
-	assign COMC = 0;
-	always @(BCD or LightTest or BLanking or LatchEnable) begin
-		if (LatchEnable == 0)	// not holding state
-			if (LightTest == 0 || BLanking == 0)
-				if (BLanking == 0)
-					SEG = 0;
-				else
-					SEG = 7'b111_1111;
-			else
-				case (BCD)
-					4'd0: SEG = 7'b111_1110;
-					4'd1: SEG = 7'b011_0000;
-					4'd2: SEG = 7'b110_1101;
-					4'd3: SEG = 7'b111_1001;
-					4'd4: SEG = 7'b011_0011;
-					4'd5: SEG = 7'b101_1011;
-					4'd6: SEG = 7'b101_1111;
-					4'd7: SEG = 7'b111_0000;
-					4'd8: SEG = 7'b111_1111;
-					4'd9: SEG = 7'b111_1011;
-					default: SEG = 7'b000_0000; 
-				endcase
-	end
+  assign COMC = 0;
+  always @(BCD) begin
+    case (BCD)
+      4'd0: SEG = 7'b111_1110;
+      4'd1: SEG = 7'b011_0000;
+      4'd2: SEG = 7'b110_1101;
+      4'd3: SEG = 7'b111_1001;
+      4'd4: SEG = 7'b011_0011;
+      4'd5: SEG = 7'b101_1011;
+      4'd6: SEG = 7'b101_1111;
+      4'd7: SEG = 7'b111_0000;
+      4'd8: SEG = 7'b111_1111;
+      4'd9: SEG = 7'b111_1011;
+      default: SEG = 7'b000_0000; 
+    endcase
+  end
 endmodule
--- TP2/cd4511.ISEProj/cd4511_VTF.v
+++ TP2/cd4511.ISEProj/cd4511_VTF.v
@@ -1,79 +1,32 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   19:52:07 08/28/2025
-// Design Name:   cd4511
-// Module Name:   D:/cd4511/cd4511_TF.v
-// Project Name:  cd4511
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: cd4511
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
+module cd4511_VTF;
 
-module cd4511_TF;
+  // Inputs
+  reg [3:0] BCD;
 
-	// Inputs
-	reg [3:0] BCD;
-	reg LightTest;
-	reg BLanking;
-	reg LatchEnable;
+  // Outputs
+  wire COMC;
+  wire [6:0] SEG;
 
-	// Outputs
-	wire COMC;
-	wire [6:0] SEG;
+  // Instantiate the Unit Under Test (UUT)
+  cd4511 uut (
+    .BCD(BCD), 
+    .COMC(COMC), 
+    .SEG(SEG)
+  );
 
-	// Instantiate the Unit Under Test (UUT)
-	cd4511 uut (
-		.BCD(BCD), 
-		.LightTest(LightTest), 
-		.BLanking(BLanking), 
-		.LatchEnable(LatchEnable), 
-		.COMC(COMC), 
-		.SEG(SEG)
-	);
+  initial begin
+    // Initialize Inputs
+    BCD = 0;
 
-	initial begin
-		// Initialize Inputs
-		BCD = 0;
-		LightTest = 1;
-		BLanking = 1;
-		LatchEnable = 0;
+    // Wait 100 ns for global reset to finish
+    #100;
 
-		// Wait 100 ns for global reset to finish
-		#100;
-        
-// Add stimulus here
-for (integer i = 0; i < 15; i = i + 1) begin
-    BCD = i;
-    #100; // El ; es necesario
-end
-
-BCD = 3;
-#100; // El ; es necesario
-LightTest = 0;
-#100;
-BLanking = 0;
-#100;
-LightTest = 1;
-BLanking = 1;
-LatchEnable = 1;
-#100;
-BCD = 5;
-#100;
-LatchEnable = 0;		
-	end
+    // Add stimulus here
+    for (BCD = 0; BCD < 15; BCD = BCD + 1) begin
+      #100;
+    end
+  end
       
 endmodule
-
